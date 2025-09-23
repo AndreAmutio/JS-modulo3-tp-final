44e73defcc671e64f997c86c81090f5117a2b8b0
Andrea Sabrina Amutio <andiamutio93@gmail.com>
2025-09-22 22:29:53 -0300

    Cambiar ícono y texto del botón para reflejar el tema actual

--- js/theme.js
+++ js/theme.js
@@ -21,12 +21,44 @@ export function initThemeToggle() {
   const cards = document.querySelectorAll('.card');
   const buttons = document.querySelectorAll('.button');
 
+   if (theme === 'dark') {
+    body.classList.remove('has-background-light');
+    body.classList.add('has-background-dark', 'has-text-white');
 
+    navbar.classList.add('has-background-success-dark');
 
+    cards.forEach(card => {
+      card.classList.remove('has-background-white');
+      card.classList.add('has-background-black-ter', 'has-text-white');
+    });
 
+    buttons.forEach(btn => {
+      btn.classList.remove('is-light');
+      btn.classList.add('is-dark');
+    });
 
+// Cambiar ícono y texto del botón para reflejar el tema actual
+    icon.classList.replace('fa-moon', 'fa-sun');
+    label.textContent = 'Claro';
+    } else {
+    body.classList.remove('has-background-dark', 'has-text-white');
+    body.classList.add('has-background-light');
 
+    navbar.classList.remove('has-background-success-dark');
 
+    cards.forEach(card => {
+      card.classList.remove('has-background-black-ter', 'has-text-white');
+      card.classList.add('has-background-white');
+    });
+
+    buttons.forEach(btn => {
+      btn.classList.remove('is-dark');
+      btn.classList.add('is-light');
+    });
+
+    icon.classList.replace('fa-sun', 'fa-moon');
+    label.textContent = 'Oscuro';
+  }
 }
 }
 
