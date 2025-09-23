72b43e9329044d026e92ebfcc531105ae7c8ee9d
Andrea Sabrina Amutio <andiamutio93@gmail.com>
2025-09-22 22:10:26 -0300

    Function para traer los estudiantes desde la API

--- js/api.js
+++ js/api.js
@@ -1,2 +1,8 @@
 // URL base para acceder a los datos de estudiantes en MockAPI
 const API_URL = 'https://68d0b153e6c0cbeb39a23376.mockapi.io/api/estudiantes'; 
+
+// Obtener todos los estudiantes desde la API
+export async function getEstudiantes() {
+  const res = await fetch(API_URL);
+  return res.json();
+}
